1e653ee304bfc6b1d29068223659b63954ed6ffa
jortiz011235 <josepho011235@gmail.com>
2013-08-20 00:08:11 -0400

    adding java hello world file

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld
+
+{
+	public static void main(String[] args)
+	
+
+	{		
+
+		System.out.println("Hello Vim \n DO NOT RUN rm c:/ \n its not fun. ");
+		System.console().readLine();
+	
+	}	
+
+}
+
+
+
